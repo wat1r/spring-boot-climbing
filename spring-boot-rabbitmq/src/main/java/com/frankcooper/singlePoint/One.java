b6a795238fb254b9998a002777fbf88a17bb2510
wangzhou <wangzhou@shengqugames.com>
2020-04-26 11:33:23 +0800

    1.test

---
+++ spring-boot-rabbitmq/src/main/java/com/frankcooper/singlePoint/One.java
@@ -0,0 +1,18 @@
+package com.frankcooper.singlePoint;
+
+/**
+ * @Date 2020/4/26
+ * @Author Frank Cooper
+ * @Description
+ */
+public class One {
+    public static void main(String[] args) {
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.UserName = "admin";//用户名
+//        factory.Password = "admin";//密码
+//        factory.HostName = "192.168.17.205";//主机名
+//        factory.VirtualHost = "";//虚拟主机(这个暂时不需要，稍后的文章里会介绍虚拟主机的概念)
+//        factory.Port = 15672;//端口
+//        IConnection conn = factory.CreateConnection();//创建连接
+    }
+}
