c1e710f32b4f7b3d76b2842589c16f75c64515fa
wangzhou <wangzhou@shengqugames.com>
2020-04-30 11:39:46 +0800

    [spring-boot-apollo] 1.run

--- spring-boot-apollo/src/main/java/com/frankcooper/SpringBootApolloApplication.java
+++ spring-boot-apollo/src/main/java/com/frankcooper/SpringBootApolloApplication.java
@@ -1,9 +1,11 @@
 package com.frankcooper;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class SpringBootApolloApplication {
 
     public static void main(String[] args) {
