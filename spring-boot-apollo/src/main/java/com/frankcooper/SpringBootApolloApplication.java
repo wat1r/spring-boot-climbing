3edc500cc2bffb2886cfd348c333c2b8ea9bea35
wangzhou <wangzhou@shengqugames.com>
2020-04-30 08:42:14 +0800

    [spring-boot-apollo] 1.init

---
+++ spring-boot-apollo/src/main/java/com/frankcooper/SpringBootApolloApplication.java
@@ -0,0 +1,13 @@
+package com.frankcooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApolloApplication.class, args);
+    }
+
+}
